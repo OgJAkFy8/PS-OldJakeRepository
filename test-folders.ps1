4feaaf91e809fb38f99dd91336aa484722850663
Erik JA <OgJAkFy8@users.noreply.github.com>
2017-10-09 07:20:34 -0400

    Update test-folders.ps1
    added some param for testing

--- test-folders.ps1
+++ test-folders.ps1
@@ -1,4 +1,70 @@
-﻿function addfold (){
+<#############################################
+Script Name: test-folders.ps1
+Author Name: Erik Arnesen
+Date: 10/5/2017
+Contact : 
+
+.SYNOPSIS
+This is a simple Powershell script to create a group of nested folders. The depth is decided by the number you add.
+
+.DESCRIPTION
+The script itself will only print 'Hello World'. But that's cool. It's main objective is to show off the cool help thingy anyway.
+
+.EXAMPLE
+./test-folders.ps1
+
+.NOTES
+Put some notes here.
+
+.LINK
+<#C:Temp\
+├───101
+│   ├───TopLev-1
+│   │   ├───NestFoldr-1-1
+│   │   ├───NestFoldr-1-2
+│   │   └───NestFoldr-1-3
+│   ├───TopLev-2
+│   │   ├───NestFoldr-2-1
+│   │   ├───NestFoldr-2-2
+│   │   └───NestFoldr-2-3
+│   └───TopLev-3
+│       ├───NestFoldr-3-1
+│       ├───NestFoldr-3-2
+│       └───NestFoldr-
+
+function test-set {
+    [CmdletBinding(DefaultParameterSetName = "BarSet")]
+    param(
+        [parameter(
+            mandatory=$true,
+            parametersetname="FooSet"
+        )]
+        [switch]$Foo,
+
+        [parameter(
+            mandatory=$true,
+            position=0,
+            parametersetname="BarSet"
+        )]
+        [string]$Bar,
+
+        [parameter(
+            mandatory=$true,
+            position=1
+        )]
+        [io.fileinfo]$FilePath
+    )
+@"
+  Parameterset is: {0}
+  Bar is: '{1}'
+  -Foo present: {2}
+  FilePath: {3}
+"@ -f $PSCmdlet.ParameterSetName, $bar, $foo.IsPresent, $FilePath
+}
+
+#>
+
+function addfold (){
 
 # PowerShell Nested Folders
 # Set Variables
