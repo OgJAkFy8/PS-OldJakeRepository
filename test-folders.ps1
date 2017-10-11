fbd54881748dd7e431c1a2472e96145019222648
Erik JA <OgJAkFy8@users.noreply.github.com>
2017-10-11 15:29:02 -0400

    Update test-folders.ps1

--- test-folders.ps1
+++ test-folders.ps1
@@ -11,13 +11,14 @@ This is a simple Powershell script to create a group of nested folders. The dept
 The script itself will only print 'Hello World'. But that's cool. It's main objective is to show off the cool help thingy anyway.
 
 .EXAMPLE
-./test-folders.ps1
+./test-folders.ps1 2 
 
 .NOTES
 Put some notes here.
 
 .LINK
-<#C:Temp\
+
+C:Temp\
 ├───101
 │   ├───TopLev-1
 │   │   ├───NestFoldr-1-1
@@ -32,47 +33,76 @@ Put some notes here.
 │       ├───NestFoldr-3-2
 │       └───NestFoldr-
 
+
 function test-set {
-    [CmdletBinding(DefaultParameterSetName = "BarSet")]
+    [CmdletBinding(DefaultParameterSetName = 3)]
     param(
-        [parameter(
-            mandatory=$true,
-            parametersetname="FooSet"
-        )]
-        [switch]$Foo,
-
-        [parameter(
-            mandatory=$true,
-            position=0,
-            parametersetname="BarSet"
-        )]
-        [string]$Bar,
-
-        [parameter(
-            mandatory=$true,
-            position=1
-        )]
-        [io.fileinfo]$FilePath
-    )
+        [parameter(mandatory=$true, parametersetname="FooSet")]
+        [switch]$FldrDpth,[parameter(mandatory=$true,position=0,parametersetname="Folder Depth")]
+        [string]$TargFldr,[parameter(mandatory=$true,position=1)]
+        [io.fileinfo]$FilePath)
 @"
   Parameterset is: {0}
   Bar is: '{1}'
-  -Foo present: {2}
+  -TargFldr present: {2}
   FilePath: {3}
-"@ -f $PSCmdlet.ParameterSetName, $bar, $foo.IsPresent, $FilePath
+"@ -f $PSCmdlet.ParameterSetName, $FldrDpt, $TargFldr.IsPresent, $FilePath
 }
-
 #>
 
-function addfold (){
+function global:ADD-TestFolders{
+
+[CmdletBinding(
+    DefaultParameterSetName = "TargetFolder", 
+    SupportsShouldProcess = $True)]
+
+param(
+    [Int]$FolderDepth = 3,
+    [STRING]$TargFldr = "c:\Temp\NestedFolders",
+    [STRING]$tpLevFldr = "TopLev",
+    [STRING]$NstFldr = "NestFoldr",
+    [STRING]$TstFile = "TestFile"
+    )
+
+    WRITE-DEBUG "`$TargFldr: $TargFldr"
+    WRITE-DEBUG "`$tpLevFldr: $tpLevFldr"
+    WRITE-DEBUG "`$NstFldr: $NstFldr"
+
+
+    <#
+[CmdletBinding(DefaultParameterSetName = "C:\Temp\test-Folders")]
+    param(
+        [parameter(
+            mandatory=$true, 
+            parametersetname="FooSet")]
+    [switch]$FolderDepth,
+        [parameter(
+            mandatory=$true,
+            position=0,
+            parametersetname="Folder Depth")]
+    [string]$TargFldr,
+        [parameter(
+            mandatory=$true,
+            position=1)]
+    [io.fileinfo]$FilePath)
+    #>
+
+@"
+  Parameterset is: {0}
+  Bar is: '{1}'
+  -TargFldr present: {2}
+  FilePath: {3}
+"@ -f $PSCmdlet.ParameterSetName, $FolderDepth, $TargFldr.IsPresent, $FilePath
+
 
 # PowerShell Nested Folders
 # Set Variables
-$TargFldr = "c:\Temp\1001"
-$tpLevFldr = "TopLev"
-$NstFldr = "NestFoldr"
-$TstFile = "TestFile"
-$i =0
+#$FolderDepth = 3
+#$TargFldr = "c:\Temp\1001"
+#$tpLevFldr = "TopLev"
+#$NstFldr = "NestFoldr"
+#$TstFile = "TestFile"
+$i = 0
 
 if(!(Test-Path $TargFldr)){
     New-Item -ItemType Directory -Path $TargFldr -Force
@@ -80,19 +110,19 @@ if(!(Test-Path $TargFldr)){
 
 Set-Location $TargFldr
 
-While ($i -le 4) {
+While ($i -le $FolderDepth) {
 $i +=1
 Set-Location $TargFldr
 New-Item -ItemType Directory -Path $TargFldr"\"$tpLevFldr"-"$i -Force
 #Set-Location .\$tpLevFldr"-"$i
 $j=0
-While ($j -le 4) {
+While ($j -le $FolderDepth) {
     $j +=1
     #Set-Location .\$tpLevFldr"-"$i
     New-Item -ItemType Directory -Path $TargFldr"\"$tpLevFldr"-"$i"\"$NstFldr"-"$i"-"$j -Force
     #Set-Location .\$NstFldr"-"$i"-"$j
     $k=0
-    While ($k -le 4) {
+    While ($k -le $FolderDepth) {
         $k+=1
         New-Item -ItemType File -path $TargFldr"\"$tpLevFldr"-"$i"\"$NstFldr"-"$i"-"$j"\"$TstFile"-"$i"-"$j"-"$k".txt" -Force
         }
@@ -111,4 +141,3 @@ Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -an
 # Delete any empty directories left behind after deleting the old files.
 Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
 }
-
