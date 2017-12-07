a7081741d481f2b3f0743cc05dd80c4f880f3ae9
Erik JA <OgJAkFy8@users.noreply.github.com>
2017-12-06 23:15:48 -0500

    Update Download_Defender.ps1
    Added header and removed some comments.  Note: this was created to reduce the amount of time the process was taking by hand.

--- Download_Defender.ps1
+++ Download_Defender.ps1
@@ -1,4 +1,22 @@
-#Download_Defender.ps1
+#requires -version 3.0
+
+<#
+.SYNOPSIS
+	Simple script to download the Windows Defender definitions.
+.DESCRIPTION
+	This script checks the version of the latest against what is available online.  If the versions don't match
+    it will download the new version.
+.EXAMPLE
+	C:\PS> .\Download_Defender.ps1
+.NOTES
+	Author: Generik
+.OUTPUTS
+	No output except for the download.
+.LINK
+	https://github.com/OgJAkFy8/PowerShell_VM-Modules/edit/master/Download_Defender.ps1
+#>
+
+
 
 # Functions
 #=======================
@@ -42,16 +60,16 @@ Set-Location $FileLocal
 
 # Get file information from Internet
 Write-Host "Downloading $FileName file information"
-#$FileTst = Invoke-WebRequest -URI $Site 
-#$OnlineFileVer = $FileTst.versionInfo.FileVersion
+$FileTst = Invoke-WebRequest -URI $Site 
+$OnlineFileVer = $FileTst.versionInfo.FileVersion
 
 $OnlineFileVer = (get-item ".\Copy mpam-feX64 - Copy.exe").versionInfo.FileVersion  #Testing
 
 #Get file information from local file
 $LocalFileVer = (Get-Item "$FileName.$FileExt").versionInfo.FileVersion
 
-<# Test to see if the file exists. 
-Download it if it does not and test to see if the latest version. #>
+<# Test to see if the file exists. Download the file if it does not exist.
+If it does exist, it checks to see if the latest version has already been downloaded. #>
 if($LocalFileVer -ne $OnlineFileVer){
     Write-Host "Getting New filename"
     $NewName = f_verFileName "$FileName.$FileExt"
@@ -62,7 +80,7 @@ if($LocalFileVer -ne $OnlineFileVer){
         }
 
     Write-Host "There is an update available.  Starting Download"
-    #Invoke-WebRequest $Site -OutFile "$FileName.$FileExt" 
+    Invoke-WebRequest $Site -OutFile "$FileName.$FileExt" 
     }
 
 Write-Host "Finished!"
