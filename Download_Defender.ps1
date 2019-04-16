3f8b381c156047b43b68b86c8deaf060025736d2
Erik Arnesen <earnesen@ymail.com>
2019-04-15 21:19:21 -0400

    Update Download_Defender.ps1

--- Download_Defender.ps1
+++ Download_Defender.ps1
@@ -1,71 +1,133 @@
 #Download_Defender.ps1
 
-# Functions
-#=======================
-Function f_tdFileName ($baseNAME){
-$t = Get-Date -uformat "%d%H%M%S"
-return  $baseNAME + "_" + $t + ".bkp"
-}
 
-Function f_verFileName ($baseNAME){
-$t = (get-item $baseNAME).VersionInfo.FileVersion
-return  $baseNAME + "_" + $t + ".bkp"
-}
-#f_verFileName $FileDL
+This does not appear to work. 
 
-Function f_tstFile ($TestFileName){}
 
-Function f_FileDownload (){
-Invoke-WebRequest $Site -OutFile $FileDL
-}
+Function Get-DefenderDownload
+{
+  <#
+      .SYNOPSIS
+      Describe purpose of "Get-DefenderDownload" in 1-2 sentences.
+
+      .DESCRIPTION
+      Add a more complete description of what the function does.
+
+      .EXAMPLE
+      Get-DefenderDownload
+      Describe what this call does
+
+      .NOTES
+      Place additional notes here.
+
+      .LINK
+      URLs to related sites
+      The first link is opened by Get-Help -Online Get-DefenderDownload
+
+      .INPUTS
+      List of input types that are accepted by this function.
+
+      .OUTPUTS
+      List of output types produced by this function.
+  #>
+  # User Modifications
+  #=======================
+  [CmdletBinding()]
+  param
+  (
+    [Object]$baseNAME,
+    # $Site = 'http://go.microsoft.com/fwlink/?LinkID=87341',
+    $Site = 'https://github.com/KnarrStudio/PS-Scripts/archive/master.zip',
+    $FileName = 'master',
+    $FileExt = 'zip',
+    $FileLocal = "$env:HOMEDRIVE\temp\Defender"
+  )
+  Enable-Tls -Tls12
+
+  # Functions
+  #=======================
+  Function Get-tdFileName
+  {
+    [CmdletBinding()]
+    param
+    (
+      [Object]$baseNAME
+    )
+    $t = Get-Date -UFormat '%d%H%M%S'
+    return  $baseNAME + '_' + $t + '.bkp'
+  }
+
+  Function Get-verFileName
+  {
+    [CmdletBinding()]
+    param
+    (
+      [Object]$baseNAME
+    )
+    $t = (Get-Item -Path $baseNAME).VersionInfo.FileVersion
+    return  $baseNAME + '_' + $t + '.bkp'
+  }
+  #Get-verFileName $FileDL
+
+  Function Get-tstFile
+  {
+    [CmdletBinding()]
+    param
+    (
+      [Object]$TestFileName
+    )
+  }
+
+  Function Get-FileDownload ()
+  {
+    Invoke-WebRequest -Uri $Site -OutFile $FileDL -Method GET 
+  }
     
 
-# User Modifications
-#=======================
-$Site = "http://go.microsoft.com/fwlink/?LinkID=87341"
-$FileName = "mpam-feX64"
-$FileExt = "exe"
-$FileLocal = "c:\temp\Defender"
 
 
+  # Begin Script
+  # =================
 
-# Begin Script
-# =================
+  # Test and create path for download location
+  if(!(Test-Path -Path $FileLocal))
+  {
+    Write-Verbose -Message 'Creating Folder'
+    New-Item -Path $FileLocal -ItemType Directory
+  }
 
-# Test and create path for download location
-if(!(Test-Path $FileLocal)){
-    Write-Host "Creating Folder"
-    New-Item -path $FileLocal -ItemType Directory 
+  # Change the working location
+  Write-Verbose -Message ('Setting location to {0}' -f $FileLocal)
+  Set-Location -Path $FileLocal
 
+  # Get file information from Internet
+  Write-Verbose -Message ('Downloading {0} file information' -f $FileName)
+  $FileTst = Invoke-WebRequest -URI $Site 
+  $OnlineFileVer = $FileTst.versionInfo.FileVersion
+
+  $OnlineFileVer = (Get-Item -Path '.\Copy mpam-feX64 - Copy.exe').versionInfo.FileVersion  #Testing
+
+  #Get file information from local file
+  $LocalFileVer = (Get-Item -Path ('{0}.{1}' -f $FileName, $FileExt)).versionInfo.FileVersion
+
+  <# Test to see if the file exists. 
+  Download it if it does not and test to see if the latest version. #>
+  if($LocalFileVer -ne $OnlineFileVer)
+  {
+    Write-Verbose -Message 'Getting New filename'
+    $NewName = Get-verFileName -baseNAME ('{0}.{1}' -f $FileName, $FileExt)
+
+    if (Test-Path -Path ('{0}.{1}' -f $FileName, $FileExt))
+    {
+      Write-Verbose -Message 'Rename local file'
+      Rename-Item -Path ('{0}.{1}' -f $FileName, $FileExt) -NewName $NewName
     }
 
-# Change the working location
-Write-Host "Setting location to $FileLocal"
-Set-Location $FileLocal
-
-# Get file information from Internet
-Write-Host "Downloading $FileName file information"
-#$FileTst = Invoke-WebRequest -URI $Site 
-#$OnlineFileVer = $FileTst.versionInfo.FileVersion
-
-$OnlineFileVer = (get-item ".\Copy mpam-feX64 - Copy.exe").versionInfo.FileVersion  #Testing
-
-#Get file information from local file
-$LocalFileVer = (Get-Item "$FileName.$FileExt").versionInfo.FileVersion
-
-<# Test to see if the file exists. 
-Download it if it does not and test to see if the latest version. #>
-if($LocalFileVer -ne $OnlineFileVer){
-    Write-Host "Getting New filename"
-    $NewName = f_verFileName "$FileName.$FileExt"
-
-    if (Test-Path "$FileName.$FileExt"){
-        Write-Host "Rename local file"
-        Rename-Item "$FileName.$FileExt" $NewName
-        }
-
-    Write-Host "There is an update available.  Starting Download"
+    Write-Verbose -Message 'There is an update available.  Starting Download'
     #Invoke-WebRequest $Site -OutFile "$FileName.$FileExt" 
-    }
+  }
 
-Write-Host "Finished!"
+  Write-Host 'Finished!'
+}
+
+Get-DefenderDownload -Verbose
