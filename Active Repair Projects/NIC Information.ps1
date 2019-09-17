46f3150a5d0742459feef5a5bb5add08191b2f88
Erik Arnesen <earnesen@ymail.com>
2019-09-17 07:04:12 -0400

    Updates

--- Active Repair Projects/NIC Information.ps1
+++ Active Repair Projects/NIC Information.ps1
@@ -1,14 +1,17 @@
-﻿$Workstation = 'localhost'
+﻿#requires -Version 3.0 -Modules NetTCPIP
+
+$Workstation = 'localhost'
 try
 {
-  $lenovo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ComputerName $Workstation -ErrorAction Stop | Select-Object -Property [a-z]* -ExcludeProperty IPX*,WINS*
+  #$NIC = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ComputerName $Workstation -ErrorAction Stop | Select-Object -Property [a-z]* -ExcludeProperty IPX*,WINS*
+  $NICs = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ComputerName $Workstation -ErrorAction Stop | Select-Object -Property * -ExcludeProperty IPX*, WINS*
 }
 # NOTE: When you use a SPECIFIC catch block, exceptions thrown by -ErrorAction Stop MAY LACK
 # some InvocationInfo details such as ScriptLineNumber.
 # REMEDY: If that affects you, remove the SPECIFIC exception type [System.Management.Automation.ValidationMetadataException] in the code below
 # and use ONE generic catch block instead. Such a catch block then handles ALL error types, so you would need to
 # add the logic to handle different error types differently by yourself.
-catch [System.Management.Automation.ValidationMetadataException]
+catch [Management.Automation.ValidationMetadataException]
 {
   # get error record
   [Management.Automation.ErrorRecord]$e = $_
@@ -27,11 +30,20 @@ catch [System.Management.Automation.ValidationMetadataException]
   $info
 }
 
-
-$lenovo.MACAddress
-
-$NICs = Get-WMIObject Win32_NetworkAdapterConfiguration | Where-Object{$_.IPEnabled -eq "TRUE"}
-Foreach($NIC in $NICs) {
-$NIC.EnableDHCP()
-$NIC.SetDNSServerSearchOrder()
+foreach($NIC in $NICs)
+{
+  $Delimeter = ':'
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'Description', $Delimeter, $NIC.Description)
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'DefaultIPGateway', $Delimeter, $NIC.DefaultIPGateway[0])
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'DHCPEnabled', $Delimeter, $NIC.DHCPEnabled)
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'DHCPServer', $Delimeter, $NIC.DHCPServer)
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'DNSHostName', $Delimeter, $NIC.DNSHostName)
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-14}{3,-14}' -f 'DNSServerSearchOrder', $Delimeter, $NIC.DNSServerSearchOrder[0], $NIC.DNSServerSearchOrder[1])
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'IPAddress', $Delimeter, $NIC.IPAddress[0])
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'IPSubnet', $Delimeter, $NIC.IPSubnet[0])
+  Write-Output -InputObject ('{0,-23}{1,-4}{2,-24}' -f 'MACAddress', $Delimeter, $NIC.MACAddress)
 }
+
+$ExternalIp = Invoke-RestMethod -Uri http://ipinfo.io/json | Select-Object -ExpandProperty ip
+Test-NetConnection -ComputerName $ExternalIp -TraceRoute 
+
