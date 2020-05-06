74882c3c79fac3f05b8bc84fc005460e7c679f72
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-05-05 22:34:59 -0400

    Update Show-DynamicMenuOfCurrentDirectory.ps1
    big overhaul

--- Menu/Show-DynamicMenuOfCurrentDirectory.ps1
+++ Menu/Show-DynamicMenuOfCurrentDirectory.ps1
@@ -1,3 +1,5 @@
+#requires -Version 3.0
+
 Function Show-DynamicMenu
 {
   <#
@@ -10,22 +12,26 @@ Function Show-DynamicMenu
 
       .EXAMPLE
       Show-DynamicMenu
-      Shows a menu based on the menu.csv file if it exists, otherwise, it shows a menu base on the files in the current directory
+      Shows a menu base on the files in the current directory. 
 
       .EXAMPLE
       Show-DynamicMenu -Folders
       Shows Menu base on the folders in the current directory
  
       .EXAMPLE
-      Show-DynamicMenu -inputFile .\test2.csv
-      If you want to change the input filename from menu.csv
+      Show-DynamicMenu -MenuFileCsv .\test2.csv
       Shows Menu base on the files in the file.
       The file must have "Name" as the title of the column. ("Name" must be on the first line)
       
-      File Format:       Output Format:
-      Name               0. Exit
-      Move               1. Move
-      Delete             2. Delete
+      MenuFileCsv Format:
+      "Name"
+      "Move"
+      "Delete"
+          
+      Output:
+      0. Exit
+      1. Move
+      2. Delete
       Select number: 
 
 
@@ -35,68 +41,97 @@ Function Show-DynamicMenu
       Or you don't want to rebuild a menu.
 
       .INPUTS
-      looks for "menu.csv" file first
+      File based on parameter - MenuFileCsv or Get-ChildItem of current Directory (folder)
 
       .OUTPUTS
       Passes or prints the selection of the menu as a name
   #>
-  [CmdletBinding()]
+  [cmdletbinding(DefaultParameterSetName = 'Files')]
   param
   (
+    [Parameter(Position = 0,ValueFromPipeline = $true,ValueFromPipelineByPropertyName = $true,ParameterSetName = 'InputFile')]
+    [AllowNull()]
+    [ValidateScript({
+          $_ -match '.csv'
+    })]
+    [String]$MenuFileCsv,
+    [Parameter(Position = 1,ParameterSetName = 'Folder')]
     [switch]$Folders,
-    [Switch]$Files,
-    [String]$inputFile = "$env:HOMEDRIVE\temp\menu.csv"
+    [Parameter(Position = 1,ParameterSetName = 'Files')]
+    [Switch]$Files
   )
-    
-  if (Test-Path -Path $inputFile){
-    $DirectoryItems = Import-Csv -Path $inputFile
-    Write-Debug -Message ("Txt File True - `n{0}" -f $DirectoryItems)
-  }
-  Else{  
-    if($Folders)
+      
+  $ans = $null
+  $exit = 'Exit'
+  $menu = [Ordered]@{}
+ 
+  Write-Verbose -Message ('{0} :: {1} :: {2}' -f $MenuFileCsv,$Folders,$Files)        
+   
+  Switch ($PsBoundParameters.keys){
+    'Folders' 
     {
       $DirectoryItems = Get-ChildItem -Directory 
-      Write-Debug -Message ("Folders Switch True - `n{0}" -f $DirectoryItems)
+      Write-Verbose -Message ('Folders Switch True - {0}' -f $DirectoryItems)        
     }
-    Else{
-      $DirectoryItems = Get-ChildItem -file
-      Write-Debug -Message ("Files switch True - `n{0}" -f $DirectoryItems)
+    'Files' 
+    {
+      $DirectoryItems = Get-ChildItem -File
+      Write-Verbose -Message ('Files switch True - {0}' -f $DirectoryItems)
+    }
+    'MenuFileCsv'
+    {
+      $DirectoryItems = Import-Csv -Path $MenuFileCsv
+      Write-Verbose -Message ('CSV File True - {0}' -f $DirectoryItems)
+    }
+    'Default'
+    {
+      $DirectoryItems = Get-ChildItem -File
+      Write-Verbose -Message ('Default Switch - {0}' -f $DirectoryItems)
     }
   }
-  $menu = @{}
-  $folderCount = $DirectoryItems.Count-1
-  for($i = 0;$i -lt $DirectoryItems.count;$i++)
+  
+  $folderCount = ($DirectoryItems.Count - 1)
+  
+  for($i = 0;($i -lt $DirectoryItems.Count);$i++)
   {
     if($i -eq 0)
     {
-      Write-Host ('{0}. {1}' -f $i, 'Exit')
-      $i++
+      $menu.Add($i,$exit)
     }
-    Write-Host ('{0}. {1}' -f $i, $DirectoryItems[$i].name)
-    $menu.Add($i,($DirectoryItems[$i].name))
-  }
-  $ans = 99
-  do{[int]$ans = Read-Host -Prompt 'Select number'
-    if($ans -ne 0)
+    else
     {
+      $menu.Add($i,($DirectoryItems[$i].name))
+    }
+    
+    Write-Output -InputObject ('{0}. {1}' -f $i, $menu[$i])
+  }
+
+  do
+  {
+    try
+    {
+      [int]$ans = Read-Host -Prompt 'Select number'
       if(($ans -ge $folderCount) -or ($ans -lt 0))
       {
         Write-Warning -Message ('Select a number from 0 to {0}' -f $folderCount)
       }
     }
+    catch
+    {
+      Write-Warning -Message ('Retry with number.')
+    }    
   } 
+  
   while($ans -notin 0..$folderCount)
  
   $selection = $menu.Item($ans)
-   <###################
-     Run Amazing code by passing $selection to the next function
-     
-     Return $selection
-  ###################>
-   
-  # Visual output for Testing
-  Write-Host 'You selected: '$selection -ForegroundColor Magenta
-    
-
+  
+  Return $selection
 }
-Show-DynamicMenu
+
+Clear-Host
+Write-Verbose -Message 'Show Dynamic Menu'
+Show-DynamicMenu  -Verbose
+
+# Visual output for Testing
+Write-Output -InputObject ('You selected: {0}' -f $selection)
