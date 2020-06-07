49b83b1aa08c5174771cb2adcd78e239732e2113
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-06-07 10:12:12 -0400

    Added-snippets

---
+++ Snippets-Partials/Show-MessageBox.ps1
@@ -0,0 +1,53 @@
+﻿$ComputerLocation = (@'
+
+ComputerName (Assest Tag)
+- {0}
+
+Department
+- {1}
+
+Building
+- {2}
+
+Room
+- {3}
+
+Desk
+- {4}
+          
+'@ -f 'ComputerName variable', 'Department variable', 'Building variable', 'Room variable', 'Desk variable')
+
+$MessageSplat = @{
+Message   = $ComputerLocation
+TitleBar     = 'Title Bar'
+}
+
+function Show-MessageBox
+{
+  [cmdletbinding(DefaultParameterSetName = 'Message')]
+  param(
+    [Parameter(Mandatory=$false,ParameterSetName = 'Message')]
+    [Switch]$YesNoBox,
+    [Parameter(Mandatory=$False,ParameterSetName = 'Input')]
+    [Switch]$InputBox,
+    [Parameter(Mandatory,Position = 0)]
+    [string]$Message,
+    [Parameter(Mandatory,Position = 1)]
+    [string]$TitleBar
+    
+  )
+  Add-Type -AssemblyName Microsoft.VisualBasic
+  
+  
+  if($InputBox){
+    $Response = [Microsoft.VisualBasic.Interaction]::InputBox($Message, $TitleBar)
+  }
+  if($YesNoBox){  
+    $Response = [Microsoft.VisualBasic.Interaction]::MsgBox($Message, 'YesNo,SystemModal,MsgBoxSetForeground', $TitleBar)
+  }
+  Return $Response
+}
+
+Show-MessageBox -YesNoBox @MessageSplat
+
+
---
+++ Snippets-Partials/Start-ApplicationTest.ps1
@@ -0,0 +1,68 @@
+﻿$PDFApplicationTestSplat = @{
+  TestFile    = 'O:\OMC-S\IT\Scripts\FastCruise\FastCruiseTestFile.pdf'
+  TestProgram = "${env:ProgramFiles(x86)}\Adobe\Acrobat 2015\Acrobat\Acrobat.exe"
+  ProcessName = 'Acrobat'
+}
+$PowerPointApplicationTestSplat = @{
+  TestFile    = 'O:\OMC-S\IT\Scripts\FastCruise\FastCruiseTestFile.pptx'
+  TestProgram = "${env:ProgramFiles(x86)}\Microsoft Office\Office16\POWERPNT.EXE"
+  ProcessName = 'POWERPNT'
+}
+$WordpadApplicationTestSplat = @{
+  TestFile    = "$env:windir\DtcInstall.log"
+  TestProgram = "$env:ProgramFiles\Windows NT\Accessories\wordpad.exe"
+  ProcessName = 'wordpad'
+}
+
+
+function Start-ApplicationTest
+{
+  param
+  (
+    [Parameter(Mandatory, Position = 0)]
+    [string]$FunctionTest,
+    [Parameter(Mandatory, Position = 1)]
+    [string]$TestFile,
+    [Parameter(Mandatory, Position = 2)]
+    [string]$TestProgram,
+    [Parameter(Mandatory, Position = 3)]
+    [string]$ProcessName
+  )
+  $DescriptionLists = [Ordered]@{
+    FunctionResult = 'Good', 'Failed'
+  }
+
+  if($FunctionTest -eq 'Y')
+  {
+    try
+    {
+      Write-Verbose -Message ('Attempting to open {0} with {1}' -f $TestFile, $ProcessName)
+      Start-Process -FilePath $TestProgram -ArgumentList $TestFile
+
+      Write-Host -Object ('The Fast Cruise Script will continue after {0} has been closed.' -f $ProcessName) -BackgroundColor Red -ForegroundColor Yellow
+      Write-Verbose -Message ('Wait-Process: {0}' -f $ProcessName)
+      Wait-Process -Name $ProcessName
+        
+      $TestResult = $DescriptionLists.FunctionResult | Out-GridView -Title $ProcessName -OutputMode Single
+    }
+    Catch
+    {
+      Write-Verbose -Message 'TestResult: Failed'
+      $TestResult = $DescriptionLists.FunctionResult[1]
+    }
+  }
+  else
+  {
+    Write-Verbose -Message 'TestResult: Bypassed'
+    $TestResult = 'Bypassed'
+  }
+  Return $TestResult
+}
+
+Do
+{
+  $FunctionTest = Read-Host -Prompt 'Perform Function Tests (MS Office and Adobe) Y/N'
+}
+While(($FunctionTest -ne 'N') -and ($FunctionTest -ne 'Y')) 
+#Start-ApplicationTest -FunctionTest $FunctionTest @PDFApplicationTestSplat
+Start-ApplicationTest -FunctionTest $FunctionTest @WordpadApplicationTestSplat -Verbose
