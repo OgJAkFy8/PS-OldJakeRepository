946ccfd5261cbc3761e660fc62b136c59455ccaf
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-11-11 21:12:34 -0500

    Working with HTML
    will need to delete many files here

---
+++ PuttyConfigReg/Convert-HashToJson.ps1
@@ -0,0 +1,75 @@
+﻿[cmdletbinding()]
+param(
+  [Parameter(Position = 1)]
+  [Object]$ThemeHash
+  ,
+  [Parameter(Position = 0)]
+  [String]$ThemeFile = '.\Themes.json'
+)
+
+
+$RgbColor = @{
+  'Black' = '0,0,0'
+  'Blue' = '0,0,255'
+  'Green' = '0,255,0'
+  'Red'  = '255,0,0'
+  'Yellow' = '255,255,0'
+  'Gray' = '85,85,85'
+  'White' = '255,255,255'
+}
+
+$TermWidth = 100
+$Beep = 2
+$BeepInd = 1
+$ThemeHash = @{
+  Saroja  = @{
+    TxtColor       = '0,255,255'
+    BkColor        = '255,0,255'
+    CrsrColor      = '255,0,0'
+    TermWidth      = 100
+    Beep           = 2
+    BeepInd        = 1
+    ThemeSelection = 'Saroja'
+  }
+  Lonnie  = @{
+    TxtColor       = '0,255,255'
+    BkColor        = '255,0,255-lonnie'
+    CrsrColor      = '255,0,0'
+    TermWidth      = 100
+    Beep           = 2
+    BeepInd        = 1
+    ThemeSelection = 'Lonnie'
+  }
+  Default = @{
+    TxtColor       = $RgbColor.Gray
+    BkColor        = $RgbColor.Black
+    CrsrColor      = $RgbColor.Gray
+    TermWidth      = 80
+    Beep           = 2
+    BeepInd        = 1
+    ThemeSelection = 'Default'
+  }
+  SIPR    = @{
+    TxtColor       = $RgbColor.Gray
+    BkColor        = $RgbColor.Black
+    CrsrColor      = $RgbColor.Red
+    TermWidth      = 100
+    Beep           = 2
+    BeepInd        = 1
+    ThemeSelection = 'SIPR'
+  }
+  NIPR    = @{
+    TxtColor       = $RgbColor.Gray
+    BkColor        = $RgbColor.Black
+    CrsrColor      = $RgbColor.Green
+    TermWidth      = 100
+    Beep           = 2
+    BeepInd        = 1
+    ThemeSelection = 'NIPR'
+  }
+}
+
+$ThemeHash |
+ConvertTo-Json -Depth 5 |
+Out-File '.\Themes.json'
+
