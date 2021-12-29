fcb16025f9927ce1f121ffdb35ef80ffac3bb745
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-12-28 22:18:36 -0600

    Update BusStop.ps1
    Completed the task

--- BusStop/BusStop.ps1
+++ BusStop/BusStop.ps1
@@ -1,20 +1,24 @@
-﻿$busseats = 50
-$buspeople = $BusStop = 0
+#requires -Version 1.0
+[int]$Seats = 0
+[int]$BusStop = 0
+[int]$Passengers = 0
 
+while($Seats -lt 49)
+{
+  $BusStop += 1
 
-while($busseats -gt 0){
-$BusStop +=1
-$geton = Read-Host 'Enter the amount of people getting on'
-$getoff = Read-Host 'Enter the amount of people getting off'
+  if($BusStop -gt 1)
+  {
+    [int]$Passengers = Read-Host -Prompt 'Enter the amount of people getting off'
+    $Seats = $Seats - $Passengers
+  }
+  [int]$Passengers = Read-Host -Prompt 'Enter the amount of people getting on'
+  $Seats = $Seats + $Passengers
+  if($Seats -gt 50)
+  {
+    Write-Host -Object ('Bus is full {0} need to stay behind.' -f ($Seats - 50))
+    $Seats = $Seats - $($Seats - 50)
+  }
 
-$busseats = $busseats - ($buspeople + ($geton - $getoff))
-
-if($busseats -lt 0){
-Write-Output ('The bus is full, {0} need to stay behind' -f $($busseats-50))
-$busseats = $busseats-50
-}
-
-Write-Host ('Empty Seats: {0} after {1} bus stop.' -f $busseats, $BusStop)
-
-
+  Write-Host -Object ('Empty Seats: {0} after {1} bus stop.' -f $(50 - $Seats), $BusStop)
 }
