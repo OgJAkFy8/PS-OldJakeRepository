6f8fde3e16bf7dbe912e1c7f0650166544e315c9
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-12-28 22:22:02 -0600

    Update PathTest.ps1

--- PathTest/PathTest.ps1
+++ PathTest/PathTest.ps1
@@ -1,13 +1,5 @@
-﻿$mypath = $PSScriptRoot
+#requires -Version 3.0
+$mypath = $PSScriptRoot
 $PSScriptRoot
 New-Item -Path $mypath -Name ('test-{0}' -f (Get-Date -UFormat %j%H%M%S)) -ItemType File
-
-
-
-
-
-
-
-
-
 
